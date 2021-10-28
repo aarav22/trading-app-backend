0c5adc5c7276604546b41723c8e7c7607f890280
Aarav Varshney <aaravvarshney22@gmail.com>
2021-10-28 16:48:05 +0530

    add sleep routes

--- api/even-start-trigger/controllers/even-start-trigger.js
+++ api/even-start-trigger/controllers/even-start-trigger.js
@@ -4,12 +4,11 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
-async function startEvent() {
-    const eventTimerArray = await strapi.api['even-start-trigger'].services['even-start-trigger'].find();
-    const eventTimer = eventTimerArray[0];
-
+async function start_event(ctx) {
     // console.log(eventTimer)
     try {
+        const eventTimerArray = await strapi.api['even-start-trigger'].services['even-start-trigger'].find();
+        const eventTimer = eventTimerArray[0];
         if (new Date() > new Date(eventTimer['time']) && eventTimer['event_started'] === "null") {
             console.log("Event started");
             // update event_started to true
@@ -35,60 +34,86 @@ async function startEvent() {
                 // strapi.io.emit('event-start', { eventStarted: false, time: strapi.eventStartTime, roundNumber: strapi.roundNumber, timer: strapi.timer });
             }
         }
+        return ctx.response.send('ok');
     } catch (error) {
         console.error("error A: ", error);
+        return ctx.response.send('error');
     }
 }
 
 
-async function timerUpdate() {
-    const eventTimerArray = await strapi.api['even-start-trigger'].services['even-start-trigger'].find();
-    const eventTimer = eventTimerArray[0];
-    const round_duration = eventTimer['round_duration_in_seconds'];
-    const number_rounds = eventTimer['number_rounds'];
-    const current_round = eventTimer['current_round'];
-    if (eventTimer['event_started'] === "true") {
-        let timer = eventTimer['current_seconds'];
-        timer++;
-        await strapi.api['even-start-trigger'].services['even-start-trigger'].update(
-            { id: eventTimer.id },
-            { current_seconds: timer },
-        );
-        if ((timer >= round_duration && current_round <= number_rounds) || current_round === 0) {
-            console.log("Round ended", current_round);
-            // To publish the prices
+async function timer_update(ctx) {
+    try {
+        const eventTimerArray = await strapi.api['even-start-trigger'].services['even-start-trigger'].find();
+        const eventTimer = eventTimerArray[0];
+        const round_duration = eventTimer['round_duration_in_seconds'];
+        const number_rounds = eventTimer['number_rounds'];
+        const current_round = eventTimer['current_round'];
+        if (eventTimer['event_started'] === "true") {
+            let timer = eventTimer['current_seconds'];
+            timer++;
             await strapi.api['even-start-trigger'].services['even-start-trigger'].update(
                 { id: eventTimer.id },
-                { current_round: current_round + 1 },
-                { current_seconds: 0 },
+                { current_seconds: timer },
             );
-            await strapi.api['stock'].controllers['stock'].publishPrices();
-            // to publish the news
-            await strapi.api['news-update'].controllers['news-update'].publishNews();
-            // recalculate the networths
-            await strapi.api['portfolio'].controllers['portfolio'].recalculateNetworths();
-            console.log("EMISSION");
-            axios.get(`https://aic-sockets-pzbiacnqaa-em.a.run.app/round-update
+            if ((timer >= round_duration && current_round <= number_rounds) || current_round === 0) {
+                console.log("Round ended", current_round);
+                // To publish the prices
+                await strapi.api['even-start-trigger'].services['even-start-trigger'].update(
+                    { id: eventTimer.id },
+                    { current_round: current_round + 1 },
+                    { current_seconds: 0 },
+                );
+                await strapi.api['stock'].controllers['stock'].publishPrices();
+                // to publish the news
+                await strapi.api['news-update'].controllers['news-update'].publishNews();
+                // recalculate the networths
+                await strapi.api['portfolio'].controllers['portfolio'].recalculateNetworths();
+                console.log("EMISSION");
+                axios.get(`https://aic-sockets-pzbiacnqaa-em.a.run.app/round-update
             ?round_number=${eventTimer['current_round']}`);
-            // strapi.io.emit('round-update', { roundNumber: strapi.roundNumber });
+                // strapi.io.emit('round-update', { roundNumber: strapi.roundNumber });
+            }
         }
+        return ctx.response.send('ok');
+    } catch (error) {
+        console.error("error B: ", error);
+        return ctx.response.send('error');
     }
 }
 
-async function timerUpdateButSleep() {
-    await new Promise(resolve => setTimeout(resolve, 59000));
-    timerUpdate();
+async function timer_update_but_sleep(ctx) {
+    try {
+        const { time } = ctx.query
+        // console.log('time: ', time);
+        await new Promise(resolve => setTimeout(resolve, time * 1000));
+        timer_update();
+        return ctx.response.send('ok');
+    } catch (error) {
+        console.error("error B: ", error);
+        return ctx.response.send('error');
+    }
+
 }
-async function startEventButSleep() {
-    await new Promise(resolve => setTimeout(resolve, 30000));
-    startEvent();
+async function start_event_but_sleep() {
+    try {
+        const { time } = ctx.query
+        // console.log('time: ', time);
+        await new Promise(resolve => setTimeout(resolve, time * 1000));
+        start_event();
+        return ctx.response.send('ok');
+    } catch (error) {
+        console.error("error B: ", error);
+        return ctx.response.send('error');
+    }
+
 
 }
 
 
 module.exports = {
-    startEvent,
-    timerUpdate,
-    timerUpdateButSleep,
-    startEventButSleep
+    start_event,
+    timer_update,
+    timer_update_but_sleep,
+    start_event_but_sleep
 };
